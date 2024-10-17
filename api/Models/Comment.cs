34ce0b2e9f2701ebcdc7326b366d36ba51e60856
Ricardo <ryukyzp@gmail.com>
2024-10-16 22:36:59 -0300

    first day

---
+++ api/Models/Comment.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
+        public int? StockID { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
